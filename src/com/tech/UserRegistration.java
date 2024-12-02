86bd01f7d12b53ce4ad708b09e331d1194f31aab
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-12-02 14:39:36 +0530

    Validate First Name Of User

--- src/com/tech/UserRegistration.java
+++ src/com/tech/UserRegistration.java
@@ -1,10 +1,34 @@
 package com.tech;
 
+import java.util.Scanner;
+
+import javax.naming.Name;
+
 public class UserRegistration {
 	public static void main(String[] args) {
 		
-		System.out.println("WelCome To User Registration System ");
+		System.out.println("WelCome To User Registration Problem ");
 		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter your First Name :- ");
+		String firstname=sc.nextLine();
+		
+		if(isValidFirstName(firstname))
+		{
+			System.out.println("Vailid first name! ");
+		}
+		else
+		{
+			System.out.println("Invalid first name. Please ensure it starts  with a capital letter and has at least 3 characters.");
+		}
+		
+		
+	}
+
+	private static boolean isValidFirstName(String name) {
+		
+        return name.matches("[A-Z][a-z]{2,}");
 	}
 
 }
