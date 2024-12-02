9bbffce01ae3596ec6d53078bbe78b6004246f3c
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-12-02 14:55:59 +0530

    Validate First Name And Last Name Of User

--- src/com/tech/UserRegistration.java
+++ src/com/tech/UserRegistration.java
@@ -14,6 +14,9 @@ public class UserRegistration {
 		System.out.println("Enter your First Name :- ");
 		String firstname=sc.nextLine();
 		
+		System.out.println("Enter your Last Name :- ");
+		String lastname=sc.nextLine();
+		
 		if(isValidFirstName(firstname))
 		{
 			System.out.println("Vailid first name! ");
@@ -23,7 +26,19 @@ public class UserRegistration {
 			System.out.println("Invalid first name. Please ensure it starts  with a capital letter and has at least 3 characters.");
 		}
 		
+		if(isValidLastName(lastname)) {
+			System.out.println("Valid Last Name");
+		}
+		else
+		{
+			System.out.println("Inavlid LastName. Please ensure it starts with a capital letter and has at least 3 characters.");
+		}
+				
+	}
+
+	private static boolean isValidLastName(String Lastname) {
 		
+		return Lastname.matches("[A-Z][a-z]{2,}");
 	}
 
 	private static boolean isValidFirstName(String name) {
